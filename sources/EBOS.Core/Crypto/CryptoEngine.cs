f5f197d311c52c1d60e18e495ddb438127639d7d
Juan Manuel Pardo Garcia <plexus.jpardo@iberostar.com>
2025-11-18 20:47:57 +0100

    Clean Code refactoring

--- sources/EBOS.Core/Crypto/CryptoEngine.cs
+++ sources/EBOS.Core/Crypto/CryptoEngine.cs
@@ -6,42 +6,57 @@ namespace EBOS.Core.Crypto;
 
 public class CryptoEngine
 {
+    private static readonly Encoding Utf8Encoding = Encoding.UTF8;
+    private static readonly Encoding AsciiEncoding = Encoding.ASCII;
+
+    private const string TokenAllowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$%&|_-?";
+
     #region Base64
     public virtual string EncryptToBase64(string plainText)
     {
-        byte[] bytes = Encoding.UTF8.GetBytes(plainText);
+        if (plainText is null)
+            throw new ArgumentNullException(nameof(plainText));
 
+        byte[] bytes = Utf8Encoding.GetBytes(plainText);
         return Convert.ToBase64String(bytes);
     }
     public virtual string DecryptFromBase64(string plainText)
     {
-        byte[] bytes = Convert.FromBase64String(plainText);
+        if (plainText is null)
+            throw new ArgumentNullException(nameof(plainText));
 
-        return Encoding.UTF8.GetString(bytes);
+        byte[] bytes = Convert.FromBase64String(plainText);
+        return Utf8Encoding.GetString(bytes);
     }
     #endregion
 
     #region AES
     public virtual Tuple<byte[], byte[]> GetSaltAndCiphertext(string cipherBase64, int nBytesSalt)
     {
+        if (cipherBase64 is null)
+            throw new ArgumentNullException(nameof(cipherBase64));
+
         return GetSaltAndCiphertext(Convert.FromBase64String(cipherBase64), nBytesSalt);
     }
     public virtual Tuple<byte[], byte[]> GetSaltAndCiphertext(byte[] cipher, int nBytesSalt)
     {
-        byte[] salt = [.. cipher.Take(nBytesSalt)];
-        byte[] cypherText = [.. cipher.Skip(nBytesSalt)];
+        if (cipher is null)
+            throw new ArgumentNullException(nameof(cipher));
+        if (nBytesSalt < 0 || nBytesSalt > cipher.Length)
+            throw new ArgumentOutOfRangeException(nameof(nBytesSalt));
 
-        return Tuple.Create(salt, cypherText);
+        byte[] salt = [.. cipher.Take(nBytesSalt)];
+        byte[] cipherText = [.. cipher.Skip(nBytesSalt)];
+
+        return Tuple.Create(salt, cipherText);
     }
     public virtual byte[] GenerateIV(int nBytes)
     {
+        if (nBytes <= 0)
+            throw new ArgumentOutOfRangeException(nameof(nBytes));
+
         byte[] iv = new byte[nBytes];
-
-        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
-        {
-            rng.GetBytes(iv);
-        }
-
+        RandomNumberGenerator.Fill(iv);
         return iv;
     }
     public virtual byte[] EncryptToAES(string plainText, string key, byte[] IV)
@@ -50,74 +65,67 @@ public class CryptoEngine
             throw new ArgumentNullException(nameof(plainText));
         if (string.IsNullOrEmpty(key))
             throw new ArgumentNullException(nameof(key));
-        if (IV == null)
+        if (IV is null)
             throw new ArgumentNullException(nameof(IV));
 
-        byte[] encrypted;
+        using Aes aes = Aes.Create();
+        aes.Key = AsciiEncoding.GetBytes(key);
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.PKCS7;
 
-        using (Aes aes = Aes.Create())
+        int ivSize = aes.BlockSize / 8;
+
+        if (IV.Length != ivSize)
+            throw new ArgumentException($"IV must be {ivSize} bytes long.", nameof(IV));
+        aes.GenerateIV();
+        Buffer.BlockCopy(aes.IV, 0, IV, 0, ivSize);
+        using ICryptoTransform encryptor = aes.CreateEncryptor();
+        using MemoryStream msEncrypt = new();
+        using CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write);
+        using (StreamWriter swEncrypt = new(csEncrypt))
         {
-            aes.Key = Encoding.ASCII.GetBytes(key);
-            aes.Mode = CipherMode.CBC;
-            aes.Padding = PaddingMode.PKCS7;
-            int ivSize = aes.BlockSize / 8;
-            if (IV.Length != ivSize)
-                throw new ArgumentException($"IV must be {ivSize} bytes long.", nameof(IV));
-            aes.GenerateIV();
-            Buffer.BlockCopy(aes.IV, 0, IV, 0, ivSize);
-            using ICryptoTransform encryptor = aes.CreateEncryptor();
-            using MemoryStream msEncrypt = new();
-            using CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write);
-            using (StreamWriter swEncrypt = new(csEncrypt))
-            {
-                swEncrypt.Write(plainText);
-            }
-            encrypted = msEncrypt.ToArray();
+            swEncrypt.Write(plainText);
         }
 
-        return encrypted;
+        return msEncrypt.ToArray();
     }
     public virtual string DecryptFromAES(byte[] cipherText, string key, byte[] IV)
     {
-        if (cipherText == null || cipherText.Length <= 0)
+        if (cipherText is null || cipherText.Length == 0)
             throw new ArgumentNullException(nameof(cipherText));
         if (string.IsNullOrEmpty(key))
             throw new ArgumentNullException(nameof(key));
-        if (IV == null || IV.Length <= 0)
+        if (IV is null || IV.Length == 0)
             throw new ArgumentNullException(nameof(IV));
 
-        string? plaintext = null;
+        using Aes aes = Aes.Create();
+        aes.Key = AsciiEncoding.GetBytes(key);
+        aes.IV = IV;
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.PKCS7;
 
-        using (Aes aes = Aes.Create())
-        {
-            aes.Key = Encoding.ASCII.GetBytes(key);
-            aes.IV = IV;
-            aes.Mode = CipherMode.CBC;
-            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-            using MemoryStream msDecrypt = new(cipherText);
-            using CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read);
-            using StreamReader srDecrypt = new(csDecrypt);
-            plaintext = srDecrypt.ReadToEnd();
-        }
+        using ICryptoTransform decryptor = aes.CreateDecryptor();
+        using MemoryStream msDecrypt = new(cipherText);
+        using CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read);
+        using StreamReader srDecrypt = new(csDecrypt);
+        string plaintext = srDecrypt.ReadToEnd();
 
         return plaintext;
     }
     public virtual string EncryptToAESBase64(string plainText, string key, byte[] IV)
     {
-        if (plainText == null)
+        if (plainText is null)
             throw new ArgumentNullException(nameof(plainText));
 
         byte[] cipherBytes = EncryptToAES(plainText, key, IV);
-
         return Convert.ToBase64String(cipherBytes);
     }
     public virtual string DecryptFromAESBase64(string cipherBase64, string key, byte[] IV)
     {
-        if (cipherBase64 == null)
+        if (cipherBase64 is null)
             throw new ArgumentNullException(nameof(cipherBase64));
 
         byte[] cipherBytes = Convert.FromBase64String(cipherBase64);
-
         return DecryptFromAES(cipherBytes, key, IV);
     }
     #endregion
@@ -125,74 +133,59 @@ public class CryptoEngine
     #region SHA
     public virtual string GenerateSHA256(string plainText)
     {
-        if (plainText == null)
+        if (plainText is null)
             throw new ArgumentNullException(nameof(plainText));
 
-        using SHA3_256 sha1 = SHA3_256.Create();
-        byte[] input = Encoding.ASCII.GetBytes(plainText);
-        byte[] hash = sha1.ComputeHash(input);
+        using SHA3_256 hasher = SHA3_256.Create();
+        byte[] input = AsciiEncoding.GetBytes(plainText);
+        byte[] hash = hasher.ComputeHash(input);
 
-        StringBuilder sb = new(hash.Length * 2);
-        for (int i = 0; i < hash.Length; i++)
-            sb.Append(hash[i].ToString("x2", CultureInfo.InvariantCulture));
-
-        return sb.ToString();
+        return BytesToHex(hash);
     }
     public virtual string GenerateSHA384(string plainText)
     {
-        if (plainText == null)
+        if (plainText is null)
             throw new ArgumentNullException(nameof(plainText));
 
-        using SHA3_384 sha1 = SHA3_384.Create();
-        byte[] input = Encoding.ASCII.GetBytes(plainText);
-        byte[] hash = sha1.ComputeHash(input);
+        using SHA3_384 hasher = SHA3_384.Create();
+        byte[] input = AsciiEncoding.GetBytes(plainText);
+        byte[] hash = hasher.ComputeHash(input);
 
-        StringBuilder sb = new(hash.Length * 2);
-        for (int i = 0; i < hash.Length; i++)
-            sb.Append(hash[i].ToString("x2", CultureInfo.InvariantCulture));
-
-        return sb.ToString();
+        return BytesToHex(hash);
     }
     public virtual string GenerateSHA512(string plainText)
     {
-        if (plainText == null)
+        if (plainText is null)
             throw new ArgumentNullException(nameof(plainText));
 
-        using SHA3_512 sha1 = SHA3_512.Create();
-        byte[] input = Encoding.ASCII.GetBytes(plainText);
-        byte[] hash = sha1.ComputeHash(input);
+        using SHA3_512 hasher = SHA3_512.Create();
+        byte[] input = AsciiEncoding.GetBytes(plainText);
+        byte[] hash = hasher.ComputeHash(input);
 
-        StringBuilder sb = new(hash.Length * 2);
-        for (int i = 0; i < hash.Length; i++)
-            sb.Append(hash[i].ToString("x2", CultureInfo.InvariantCulture));
-
-        return sb.ToString();
+        return BytesToHex(hash);
     }
     public virtual bool VerifySHA256(string plainText, string expectedHash)
     {
-        if (expectedHash == null)
+        if (expectedHash is null)
             throw new ArgumentNullException(nameof(expectedHash));
 
         string computed = GenerateSHA256(plainText);
-
         return FixedTimeEquals(computed, expectedHash);
     }
     public virtual bool VerifySHA384(string plainText, string expectedHash)
     {
-        if (expectedHash == null)
+        if (expectedHash is null)
             throw new ArgumentNullException(nameof(expectedHash));
 
         string computed = GenerateSHA384(plainText);
-
         return FixedTimeEquals(computed, expectedHash);
     }
     public virtual bool VerifySHA512(string plainText, string expectedHash)
     {
-        if (expectedHash == null)
+        if (expectedHash is null)
             throw new ArgumentNullException(nameof(expectedHash));
 
         string computed = GenerateSHA512(plainText);
-
         return FixedTimeEquals(computed, expectedHash);
     }
     #endregion
@@ -200,13 +193,14 @@ public class CryptoEngine
     #region Token
     public virtual string GenerateUniqueKey(int maxSize = 10)
     {
-        const string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$%&|_-?";
-        char[] chars = allowedChars.ToCharArray();
-        int size = maxSize;
-        byte[] data = new byte[size];
+        if (maxSize <= 0)
+            throw new ArgumentOutOfRangeException(nameof(maxSize));
+
+        char[] chars = TokenAllowedChars.ToCharArray();
+        byte[] data = new byte[maxSize];
 
         RandomNumberGenerator.Fill(data);
-        StringBuilder result = new(size);
+        StringBuilder result = new(maxSize);
         foreach (byte b in data)
             result.Append(chars[b % chars.Length]);
 
@@ -214,29 +208,40 @@ public class CryptoEngine
     }
     public virtual string CreateTokenHMACSHA256(string plainText, string secret)
     {
-        secret ??= "";
-        var encoding = new ASCIIEncoding();
-        byte[] keyByte = encoding.GetBytes(secret);
-        byte[] messageBytes = encoding.GetBytes(plainText);
-        using var hmacsha256 = new HMACSHA256(keyByte);
-        byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+        secret ??= string.Empty;
+        byte[] keyBytes = AsciiEncoding.GetBytes(secret);
+        byte[] messageBytes = AsciiEncoding.GetBytes(plainText);
 
-        return Convert.ToBase64String(hashmessage);
+        using HMACSHA256 hmacsha256 = new(keyBytes);
+        byte[] hashMessage = hmacsha256.ComputeHash(messageBytes);
+
+        return Convert.ToBase64String(hashMessage);
     }
     public virtual bool VerifyTokenHMACSHA256(string plainText, string secret, string tokenBase64)
     {
-        if (tokenBase64 == null)
+        if (tokenBase64 is null)
             throw new ArgumentNullException(nameof(tokenBase64));
 
         string computedToken = CreateTokenHMACSHA256(plainText, secret);
-
         return FixedTimeEquals(computedToken, tokenBase64);
     }
     #endregion
 
+    private static string BytesToHex(byte[] bytes)
+    {
+        if (bytes is null)
+            throw new ArgumentNullException(nameof(bytes));
+
+        StringBuilder sb = new(bytes.Length * 2);
+        for (int i = 0; i < bytes.Length; i++)
+            sb.Append(bytes[i].ToString("x2", CultureInfo.InvariantCulture));
+
+        return sb.ToString();
+    }
+
     private static bool FixedTimeEquals(string a, string b)
     {
-        if (a == null || b == null)
+        if (a is null || b is null)
             return false;
         if (a.Length != b.Length)
             return false;
