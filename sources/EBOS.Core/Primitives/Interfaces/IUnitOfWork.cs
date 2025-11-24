8bd0d5fa0e895be83e3e4ea50c7610204468a7a0
Juan Manuel Pardo Garcia <plexus.jpardo@iberostar.com>
2025-11-24 14:01:20 +0100

    Converted the Unit of Work to a transactional document.
    It will contain:
    
    BeginTransactionAsync
    EndTransactionAsync
    CommitAsync
    RollbackAsync
    
    This generates a new revision.

--- sources/EBOS.Core/Primitives/Interfaces/IUnitOfWork.cs
+++ sources/EBOS.Core/Primitives/Interfaces/IUnitOfWork.cs
@@ -3,4 +3,9 @@
 public interface IUnitOfWork
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+    Task CommitAsync(CancellationToken cancellationToken = default);
+    Task RollbackAsync(CancellationToken cancellationToken = default);
+    Task EndTransactionAsync(CancellationToken cancellationToken = default);
 }
