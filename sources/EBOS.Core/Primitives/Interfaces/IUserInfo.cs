0b05f5083093448b9d5320cc361956dab6519afb
Juan Manuel Pardo Garcia <plexus.jpardo@iberostar.com>
2025-12-01 23:43:46 +0100

    Added IUserInfo

---
+++ sources/EBOS.Core/Primitives/Interfaces/IUserInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace EBOS.Core.Primitives.Interfaces;
+
+public interface IUserInfo
+{
+    int Id { get; set; }
+    string Username { get; set; }
+    string Name { get; set; }
+    int LanguageId { get; set; }
+    string Email { get; set; }
+    int ConnectionId { get; set; }
+}
